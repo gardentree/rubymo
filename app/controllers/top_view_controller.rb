5327ed8cfe9961d52f0a60d655fcfd65c393fef3
GardenTree <takahiro.ueki@gmail.com>
2013-05-01 21:05:54 +0900

    Refactor top view controller

--- app/controllers/top_view_controller.rb
+++ app/controllers/top_view_controller.rb
@@ -6,25 +6,29 @@ class TopViewController < UIViewController
   end
 
   def viewDidLoad
-    action_button = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemBookmarks, target:self, action:'action_tapped')
-    navigationItem.title = 'rubymo'
-    navigationItem.leftBarButtonItem = action_button
+    lambda {
+      action_button = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemBookmarks, target:self, action:'action_tapped')
+      navigationItem.title = 'rubymo'
+      navigationItem.leftBarButtonItem = action_button
+    }.call
 
-    image   = UIImage.imageNamed('bg-menuitem.png')
-    pressed = UIImage.imageNamed('bg-menuitem-hightlighted.png')
-    star    = UIImage.imageNamed('icon-star.png')
+    lambda {
+      image   = UIImage.imageNamed('bg-menuitem.png')
+      pressed = UIImage.imageNamed('bg-menuitem-hightlighted.png')
+      star    = UIImage.imageNamed('icon-star.png')
 
-    item1 = AwesomeMenuItem.alloc.initWithImage(image,highlightedImage: pressed,ContentImage: star,highlightedContentImage: nil)
-    item2 = AwesomeMenuItem.alloc.initWithImage(image,highlightedImage: pressed,ContentImage: star,highlightedContentImage: nil)
-    item3 = AwesomeMenuItem.alloc.initWithImage(image,highlightedImage: pressed,ContentImage: star,highlightedContentImage: nil)
-    item4 = AwesomeMenuItem.alloc.initWithImage(image,highlightedImage: pressed,ContentImage: star,highlightedContentImage: nil)
+      item1 = AwesomeMenuItem.alloc.initWithImage(image,highlightedImage: pressed,ContentImage: star,highlightedContentImage: nil)
+      item2 = AwesomeMenuItem.alloc.initWithImage(image,highlightedImage: pressed,ContentImage: star,highlightedContentImage: nil)
+      item3 = AwesomeMenuItem.alloc.initWithImage(image,highlightedImage: pressed,ContentImage: star,highlightedContentImage: nil)
+      item4 = AwesomeMenuItem.alloc.initWithImage(image,highlightedImage: pressed,ContentImage: star,highlightedContentImage: nil)
 
-    menu = AwesomeMenu.alloc.initWithFrame(UIScreen.mainScreen.bounds,menus: [item1,item2,item3,item4])
-    menu.delegate = self
-    menu.startPoint = CGPointMake(40.0, 360.0)
-    menu.menuWholeAngle = Math::PI * 0.625
+      menu = AwesomeMenu.alloc.initWithFrame(UIScreen.mainScreen.bounds,menus: [item1,item2,item3,item4])
+      menu.delegate = self
+      menu.startPoint = CGPointMake(40.0, 360.0)
+      menu.menuWholeAngle = Math::PI * 0.625
 
-    view.addSubview(menu)
+      view.addSubview(menu)
+    }.call
 
     # self.view.backgroundColor = UIColor.blueColor
   end
